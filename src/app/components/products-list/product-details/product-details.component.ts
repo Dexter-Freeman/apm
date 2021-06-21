8fef15663cb9c7de6690f7e4194ec2dab6a4aebf
Dexter <vse.pisma.tut@gmail.com>
2021-06-21 21:26:57 +0300

    Passing parameters to a Route, CanActivate Guard

--- src/app/components/products-list/product-details/product-details.component.ts
+++ src/app/components/products-list/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from '../products.interface';
 
 @Component({
   selector: 'pm-product-details',
@@ -8,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
 export class ProductDetailsComponent implements OnInit {
 
   pageTitle: string = 'Product details';
+
+  @Input()
+  product: IProduct | undefined;
   
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.pageTitle += `: ${id}`;
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
